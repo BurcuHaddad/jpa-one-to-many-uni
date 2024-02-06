044614d364455958cbf51951e9d85d15a38eeed4
BurcuHaddad <burcuhaddad@gmail.com>
2024-02-06 08:48:06 +0300

    first commit

---
+++ cruddemo/cruddemo/src/main/java/com/devbh/cruddemo/dao/AppDao.java
@@ -0,0 +1,23 @@
+package com.devbh.cruddemo.dao;
+
+import com.devbh.cruddemo.entity.Course;
+import com.devbh.cruddemo.entity.Instructor;
+import com.devbh.cruddemo.entity.InstructorDetail;
+
+import java.util.List;
+
+public interface AppDao {
+    void save(Instructor theInstructor);
+    Instructor findInstructorById(int theId);
+    void deleteInstructorById(int theId);
+    InstructorDetail findInstructorDetailById(int theId);
+    void deleteInstructorDetailById(int theId);
+    List<Course> findCourseByInstructorId(int theId);
+    Instructor findInstructorByIdJoinFetch(int theId);
+    void update(Instructor tempInstructor);
+    void update(Course tempCourse);
+    Course findCourseById(int theId);
+    void deleteCourseById(int theId);
+
+}
+
